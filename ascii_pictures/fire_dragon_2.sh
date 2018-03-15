9127ed0a320ad85b86fe485c73f8abfd0e0e37f3
Keon Ye <jnuyeyizhou@gmail.com>
2018-03-16 01:37:29 +0800

    add fire_dragon_2.sh script into the project

---
+++ ascii_pictures/fire_dragon_2.sh
@@ -0,0 +1,58 @@
+#!/bin/bash --login
+clear
+cat << "EOF"
+
+
+
+                                         XN                                          XdOOWW                                                        
+                                      NK0xk                                          W0xO0XW                                                       
+                                    NK0OkK                               KOkOKXXN      OdOOON                                                      
+                                 WXO0OkxN                                NKkxk0000KXXKXKoxkOkX                                                     
+                              WK0OOOOxdxX                                   WKdkOOkxk00OOOOOOk0XW                   NOKN                           
+                             XO0o:;;:oOkk                                     NxdkOOOO0KXX0OOOO0KX                   OO00W                         
+                            Kkkc,;,;;;okdW                                     XlxOOOdOdkKX0xkOk0ON                  XxOO0                         
+                          NOkx:,;;;;;;;kdX                                    KdddkOOol,c0k0kdOkkOW                  KoxddOXW                      
+                         XOOo;;;;,cl:;,okk                                  WkdxxxxOOkdddKOdxdOOOx0W               WOdkkddkOO0X                    
+                       WKOx:,;;;,:lllc;cOdN                                WkdxxxoxOkkOOOOkkOOOOOOOOKW             0dx:;,:;:lk0K                   
+                     X0Okc;;;;;;;llllll;xkkW                               koxxxxodxOoodddxOOOOkOOOOkOK            xk:;;,;;;;;x0X                  
+                   NKOxl;;;;;;;,:llllllccOdO                              Koxxxxxxoooxocllcl:dkOOkOOkx0W          Odx,;;,:lc;;,kON                 
+                 W0kd:,;;;:;;;;;llllllll:lOd0                             kxxxxxxxkkdcx:.ckdookkkO00OkN          Xokc,;;,;llc;;:OON                
+                 Oko,;;;:cc;;;;:lllllllll;lOx0N                          KoxxxxxOOOOOxcx'.;xkcxKW                xxk,;;;;;lll:;,lkO                
+                Xkd,;;:cllc;;;,:lllllllllc;:kOkkO0KW                     kddxxxOOOOOOkcko,,'c.oxWNOKW    OK     XoOo,;;;;;llll:;,cxkX              
+               Nkd,;;cllllc;;;'clllllllllll:;:cloxkkKW                  WdxxxxkOOOOOOO0kxkooddxkX NloNKx000W    kkO:;;;;;,llllc;;,:dxk0N           
+              Wkd,;;:lllll:;;;,lllllllllllllc;;;;;:dxkW                 0lddxkOOOOOOKK  NK0KXW    WOl:;l0NW    NdOo,;;;;;,lllllc;;;,;:xkN          
+             Wkd;;;;clllll:;;;;lllllllllllllll:;;;;;oxxW         NW     OxxxxOOOOOOOX0             NXkxKX000000kko,;;;;;;'clllllc;;;;;;k0          
+            Wko,;;;cllllll:;;;:llllllllllllllll:;;;;;lkdK    WX0OxkK    OxdxkOOOOOOOX0               WWN0dkOdlc:;,;;;;;;;':llllllc;;;;'dOW         
+           Xkl,;;;clllllllc;;;;lllllllllllllllllc:;;;;cOxO0OkkkOOOOxkN  OxxxkOOOOOOOX0W    XOO0X       XdOd::::::;;;;;;;;':lllllllc;;;,lkK         
+         W0kc,;;;cllllllllc;;;;llllllllllllllllllc;;;;;:xOkdoolc:oOOxxXKkxxxxOOOOOOOKON  XxxkOOO0XW   KkOl;cllllllc;;;;;;':llllllllc:;;;kkW        
+       WKkd;,;;;clllllllllc;;;;llllllllllllllllllll:;;;;clcllllll;:doodkkkOkkkxOOOxkOKOXkdOOdccdOOk00OkOl;llllllllll:;;;;':llllllllllc;,oOK        
+     WKkdc,;;;:llllllllllll:;;;llllllllllllllllllllc;;;;cllllllddlccxOOOOOOOOOOkOOxOO0KKKOx:;:c:;cokkOOl;llllllllllll:;;;'cllllllllllll;;okN       
+    Xko:;;;;;clllllllllllllc;;,lllllllllllllllllllllc;;;:lllld0KKXXX0OOOOOOOOOOOOOOOOOOOO0KOkkxo:;;;:cccllllllllllllll;;;,lllllllllllllc;,okK      
+   0xc,;;;;:llllllllllllllll:;,llllllllllllllllllllll:;;;llcx0OOKK00OOOOOOOOOOOOOOOOOOOOOOOO0KXNNk;;;:llllllllllllooll:;;;llllllllllllllc;;lxON    
+  Kx:,;;;;:llllllllllllllllll;':lllllllllllclllcllllll;;cdkxokOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOkOO00Xl;;cllllllllll:k0kxcc;;;lllllllllllllllc;;;xkX   
+ Nxl,;;;;:cllllllllllllllllll:,:llllllllll:okkOo;llccodO0OOOkxxkkOOOOOOOOOOOOkkkOOOOOOOOOOOOkdxkOxd::lllllllldoodNW0olc;,:llllllllllllllll:;;;xkN  
+ Ox;;;;;;llllllllllllllllllllc;:lllllllllddooNNklxxdokOOOkxdoooodkOOOOO0KKXXXXXXXKKK00OOOOOOOOdddok0xllllllck0xkoKXdxxc,,llllllllllllllllll;;;:kkW 
+Nxc,;;;;:lllllllllllllllllllll:;llllllxKW Nkxokklod00okxocloxdxkOOO0KXNWWWWWWWWWWWWWNNNX0OOOOO0xdxxkO0xollcoXOoccollxxkd:llllllloolclllllll:;;,lkK 
+0o,;;;;;clllllllcclloxxxxdlllll;lllldN    kkkxcloolkNdxd,:dxxxxOO0XNWNWWWWWWWWWWWWWWWNNWWNX0OOO0ddddxOOxxdxdddddxolcckNdclllokXKNWWNKOxlcll:;;;:k0 
+xl,;;;;;lllllxO0KNW        XOdlc;llOW     OkOWOdxOOOkX  klxxxxkOKNWWWWWWWWWWWWWWWWWWWWWWWNWNX0OO0loddxdokxxxxxxkKKllcdl:llxXW  d;0W    WKOd:;;;,dxX
+dl,;;;;:lllxN                 Xo:cx         XXKKX       KoxxxxkKWWWWWWWWWWWWWWWWWWWWWWWWWWWWNNOOKx;cxddxxxxxxxK   0lc,;cdX    Xc,;K        Kd:;,cxK
+xl;;;;;clxX                    WklX                 WK0Odoxxxk0NNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNOOkkW N0ddxxk0N     x;,l0W   0Xd,dl;oX        Xc;;xk
+dc;;;;;lK                        WW             WXK0kxkxxxxxxONWWNWWWWWWWWWWWWWWWWWWWWWWWWWWWWNKOkxXW  WKXN        XoOW    X:::,OKx;lNW       Nc,xk
+xl;;;:OW                                      NKKKKOOOOOxxxxxXNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNOkx000KKW                  x,xl,kKKccldN       0,dd
+xl;ckW                                       X0NNNKOOOOOkdddONWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNN0xxOOXNX0KX                d;O0o0KKolkNW  W     ddx
+0o0W                                        KKNNNN0OOOOOOodd0WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNNOdxOOXNNNNK0N              x,kKKKKKKXW W0xkWW   KdO
+WK                                  WXXKKK0k0NNNK0OOOOOOOlxd0NWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWXkdOOOOKNNNNNK0W        NOOx;,dKKKKKXW  WXW      OoN
+                                  NK0K00OkdxX0K0OOOOOOOOklox0KNWWNWWWWWWWWWWWWWWWWWWWWWWWWWWWWN0kdOOOOO0KXX0KKK        W:,,,;kKKKKKN            NN 
+                                 X0X0OOOOOxX0OOOOOOOOOOOkdcoO0KNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNKklkOOOOOOOOOOOK0        Wc,c00KKKKKKXW              
+                                WOKOOOOOOOxKOOOOOOOOOOOOxxo:x00KXNWWWWWWWWWWWWWWWWWWWWWWWWWNXOxodOOOOOOOOOOOOOO       WO;,cKKKKKKKXW               
+                                kdkxkkOOOkoOOOOOOOOOOOkxxxdl:d000KXNNWWWWWWWWWWWWWWWWWWWNNXKklcoxkOOOOOOOOOOOOO      X0kxldKKKKXXW                 
+                                koxxxxxxxxloxkkkkkkkkxxxxxdlc:d00000KKXXXNNNNNNNNNNNNXXKK0kdlldxxxxxkkOOOOOOOkxXK000kkOOOkx0XNW                    
+                                Woxxxxxxxxxolxxxxxxxxxxxxxocccclk00000000000KKKKKK00000OxoccldxxxxxxxdxxxxxxookOOOOOOkkkxOW                        
+                                 NkkkxxxxxxxooodxxdxxxxddolcccccclkO0000000000000000kdlccccccloodddddxxxxdoodkkkkOOOOO00N                          
+                                  WKOxkkkkxxxolll::clllccccccccclxkkkkkOOOOOOOkxkkxlcccccccccccccc:cc::lxxxkkkOOOXKKXNWW                           
+                                     WK0kxkkodddxddlc:cccccccc:dkkkkkkkkkOOkO00OkkxkOOklcccccccllllllodooxkdxk000NW                                
+                                       W0XWW0d0NWKodOKklccccccclxxxxxxkkOKKN      NXXNdccccccccoxdxxdXWN00XKkOKX                                   
+                                       0KKKOxXXXKOKWNX0OdcccclokNNNNXN                xoclllcccloddldKKXXXkOXOOON                                  
+                                            WK0KKkkxkk00kdx0X                               NXXXXXXXXKXXXXXW                                       
+
+EOF
