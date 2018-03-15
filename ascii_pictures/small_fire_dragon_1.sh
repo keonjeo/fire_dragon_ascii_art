1f6becf385ab76b6a3e6b2f48ec1aa8c9fbfa96a
Keon Ye <jnuyeyizhou@gmail.com>
2018-03-16 01:38:54 +0800

    add small_fire_dragon_1.sh script into the project

---
+++ ascii_pictures/small_fire_dragon_1.sh
@@ -0,0 +1,58 @@
+#!/bin/bash --login
+clear
+cat << "EOF"
+
+
+
+                       WKOxdddxk0X                                                         
+                   WX0kdddddddddddxOKW                                                     
+                 XOxddddddddddddddddddOX                                                   
+               NxdddddddddddddddddddddddkN                                                 
+              WN0dddddddddddddddddddddddddX                                                
+             WN0:ddddddddddddddddoddoddddddX                                               
+             ll.odddddddddddddddddOXc.:ddddxW                                              
+            O  ;ddddddddddddddddd0  l  ;dddd0                                              
+            :.:dddddddddddddddddk  0    ddddx                                              
+         W0xoodddddddddddddddddl.;.    .dddddN                                             
+        Kxddddddddddddddddddddd,.     ;ddddddX                                             
+       Oddddddddddddddddddddddd:,;'',;oOddddxX                                             
+      KddddddddddddddddddddddddddllccldddddddN                                     N       
+      OddddddddddddddddddddddddddddddddddddddW                                    NO       
+      Wxddddddddddddddddddddddddddddddddddddd                                    XcoW      
+       Wkdddddddddddddddddddddddddddddddddddd                                 N  x::k      
+        WxoxoddddddddddddddddddxdoollllddddddW                              Ndoxl:::lW WO  
+  kxk0XW WOoc:cclllllllllccc::cxc::clddddddddX           WXK0OK            Wc::::::::ddlo  
+  kdddddkOK0xoxdddddddddddooollxcldddodddddddO     NXK0kxddddxN            N::::::c:::::c  
+0kdodddddddddodddddxxxxxxxxxddddddolldddddddddOOkxdddddddddloxXW            Oxl:::0c:::::X 
+XOddddddddddddddddddxxxxddddxxkkkdoodddddddddddddddddddddddllodx             WKOckK::::::k 
+  NkdddddddddddddddkkOOOOOO000000KX0xddddddddddddddddddddddddkK               NX0Xk::::::co
+    XkddddddddddddkXK000000000KXXXXXXOdddddddddddddddddddddxX               WNNXXXO::o:::::
+      XxdddddddddxXXXXK0000KXXXXXXXXXXKxdddddddxdddddddddx0W                WXXXXXXcdKc:::c
+        Kdllooool0XXXXXXXXXXXXXXXXXXXXXXxdddddollllooookKW                  WXXXXXXKXXXo:cK
+          XklllloXXXXXXXXXXXXXXXXXXXXXXXXkdddddollld0X                      WXXXXXXXXXXk:N 
+            WKkdkXXXXXXXXXXXXXXXXXXXXXXXXXxdddddddddN                        WNXXXXXXXXxd  
+                KXXXXXXXXXXXXXXXXXXXXXXXXXXxddddddddxW                        WNXXXK0KklN  
+               WKXXXXXXXXXXXXXXXXXXXXXXXXXXKdddddddddO                          WWWddd0W   
+                KXXXXXXXXXXXXXXXXXXXXXXXXXXXkdddddddddK                           Xddx     
+                KXXXXXXXXXXXXXXXXXXXXXXXXXXXKdddddddddxW                          kddx     
+                XXXXXXXXXXXXXXXXXXXXXXXXXXXXXxddddddddd0                         0dddk     
+                NXXXXXXXXXXXXXXXXXXXXXXXXXXXXxdddddddddxW                       Odddd0     
+                 KXXXXXXXXXXXXXXXXXXXXXXXXXXXkdddddddddd0                     NkdddddN     
+                 NXXXXXXXXXXXXXXXXXXXXXXXXXXXkddddddddddx                   W0ddddddO      
+                 NOXXXXXXXXXXXXXXXXXXXXXXXXXXxdddddddddddKW              WKOdddddddxW      
+               KkddkXXXXXXXXXXXXXXXXXXXXXXXXXddddddddddddoxk0KXNNNXXKK0OxdddddddddkN       
+             Nkdddddx0XXXXXXXXXXXXXXXXXXXXXXOddddddddddoodddddddddddddddddddddddxON        
+            0dddddddddx0XXXXXXXXXXXXXXXXXXXKdddddddddddddddddddddddddddddddddddkKW         
+           KdddddddddddddxO0KKXXXXXXXXXXXK0xdddddddddddddoddddddddddddddddddxOKN           
+           OdddddddddddlllloxO00000000000kdddddddddddddddodddddddddddddddxk0KXW            
+           Xdddddddddddlllllllod00OO000OooddddddddddddddddxxxxxxxddxxkO0KXXNW              
+            XxdddddddddllllllldX   WNNNolddddddddddddddddO0KXXXXXXXXXXXXNN                 
+            WXoloodddolllllllO         NodddddddddddddddKNXXKKXXXNNNNW                     
+       WX0OxddddlccllllllllllX          WdddollooooolldX                                   
+    NNX0xdddddddddddolllllllll0         KdddddolllloooO                                    
+   NNXXNNNxkxdddddoollllllllllO         xddddddddddddddO                                   
+    WNNNKKXX0dddddddxxxkkO0KXW          kd0KOdkK0xd0NXdO                                   
+WNNNNWNNWNNNNWNNNNWNWN                  W0XNNKkNNOONNXX  NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNXNN 
+  WWWW         W                          WXKK XXWNKX     W WW                             
+
+EOF
