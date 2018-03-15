b133bf26eaa13e18f7d8c098744ae6d43291b0a6
Keon Ye <jnuyeyizhou@gmail.com>
2018-03-16 01:40:03 +0800

    add two_fire_dragons.sh script into the project

---
+++ ascii_pictures/two_fire_dragons.sh
@@ -0,0 +1,47 @@
+#!/bin/bash --login
+clear
+cat << "EOF"
+
+
+
+                                                                                                                                                                                  
+                                                                                                                                                                                  
+                                                                                                                                                                                  
+                                                              WW                                                                                           W                      
+                                WWX0N   WWWWWW               WXdkN                               W   WWNNW                        W     W     WWWWNXKK00000000KNW                 
+                              WW0o:kW  WWOxOKNWWW            WWO:ckNW                            WWWW0xdKW           WWWWNWW      WWNXXK0000OOOOkdddooodxkOO0KKNWW                
+                             WNd::OWW  WWWXxood0NWW            Wx::lN                            WOkxdooKWWXkKW     WWXOdOW     WWKkxkkxxddxxxdolc:;,,;;;;;;;:::oXW               
+                          W WNo::oWWW  WW WWXo::cxKWWWWW        Xc::dWW                         WKKdooooKNKOcxOKWW WNOddoKW     Nkxkkxxddxddc:;;;;;;;;;;;;;;:okKWW                
+                            Wx;;;OW         WNo;,;;cx0xOW      WNc;:;K                         WNkOodoookxkcccccok0kdddkKWW     KxkO00Odcll;;;;;;;;;;;cldkOXW                     
+                           Wk;;;,0           WKd:,,,;;;'c0WW    Nc;;;xW                       WW0xxlxoloxoclcllcllloooo0W       WWWW   Klo;;;;;;;;;;,:oxKWW                       
+                        WKxc;,;;,0            WWO;,;;;cc;;:xXWWWO,;;;:0W                     WWKddolodxxo::d:,;cooloooxNW             W0dc;;;;;;;;;;;;;;;cokXW                    
+                    WWKxc;;;;,;;;dWWWW       WKo;;ldc;;:,,;;;coO:.,;;,;ckNWW                 NdlolloxxxdldkxolldxololcOW              Wxd;;;;;;;;;;;;;;;;;;,,cxKW                 
+                  WNOc;;;;;;,;::,'xkxW      Wk;;;:dddolcccll,';l'.,;;,;;;cxXWW               kllcloddddxdxddooxxcclllcc0W           WNkdo;;;;;;;;;;;;;;;,,''''',:xKWW             
+               WWNxc;;;;;;;,;;;,,'.,xWW   WWx;;:ddoooodxxdoodxx,.',;;,;;;:::oKW            WXo::l:ckdxxxxdoddooo:cooc::dXXNW      WWKxoc;;;;;;;;;;;,,,,,''',''',,',l0WW           
+               WOc;;;;;;;,,,,,,,,,;'.xWWWWNd;;okOkxXNNWWWWNNWO,.',,,;;;;;:::::dKW        WWNklcldodddkxdoodkkxddxxxxoddxooKWWWWWWWKkoc;;;;;;;;;,,,,,'';coolc:;;,,,,;:xXWW         
+     WNNW      K:;;;;;;,,,''',''',;,..'clll;;dOOOxXW     WN0d'.,,,,,,,;,;;::::::kW       WWKdodxolodox0xONXkkKXKOkddxxxxlOXXOOk00dl:,,,,,,,,''''''''''',cONWWWWWNNNWWXkxN         
+    WW0okN    Wk;;;;;;,,'.......',;;;'....,ckOOOOll;,;ll:,..'',,,,,,,,,;,,;:::::;0WW      WWX00OOolcc0oOWWW0,,xKXNX0xdxxxdoldKOc::,'''''''''''''''''''''',:xX    WWX0xdkW         
+     WXoo0WWW X:,;;;,,:kx,.,cx:';;;;;'',;;ckkOOkx;;,........',,,;;,,,,,,,,,;:::::cKW            WKddkOKW  WKlcxxkNNNXkxddxxodxxo''''','''''''''''''''''''''':ONWW0kxdokXWW        
+      NdoOWWWWk'',;;,:0WNO0WW0,',;;;:;;;;:kOOOOOd;;;:,...,;,,,,;:;;,,,,,,,,;;::::;;kW           WW   WW   WKKklldXNXX0XXkxxkkxl;'''''''''''''',:;'''''''''''',cONkddooo0W         
+     WKoodkXWWO,.,;;;kW W    0,...,,,;:c;dOOOOOOd;,,;;'.,::::::cxKKOo;,,ldc;;;;::;',kW            WWWWW  WWXKdllkXXXNK00Oxxxxxo;,''','''''''''';xKkd:,''''''''',d0xddlodKW        
+     WXxoood0WXc,:;;:N       X;.....'oXdlOOOOOOOx;cc;;,,;;;,,'.....';c::xWW0c;;;::'.'kWW       WWXdlookNWW  Wdlc0NNNNNX0Odxdlloodo:'cl;''''''''';xN WN0ko:,',,,,,cdxkoodKW        
+      WWKodk0WWK0l,;dW     WW0,..:dONWKlkOOOOOOOOc;,,;,;;;;,...........;OW WKc;::clx:'kW       WXooo::oKWWWNxlclXNNNNNNNNOddlllodd;'cXNk:'''''''',oXW WWWWN0kl;;,,lxod0NW         
+        NxO00WW Wo,;OW    WWXlcoKW   WxdOOOOOOOOOo,;;,,,,;,',:'.........,OWWW0:;:xWWNxoN       W0l:occccodxolcc0NNNNNNNKxxddoodxdc'':0  W0d:,''''',oXW    WWXoooddlloOWW          
+      WNkdOxoxOKNo,;0W      WNNW     NoxOOOOOOOOd::;;,,,,;,;;;;;;',,,,,,lxWWWWO::oW             WNxololooooooO0NNNNNNNKdxdkkkxdodd:':0W    WXkoc;,,,oN     WNdoxkkolo0W           
+      W0lxkdooooxk;;K             WWKo:OOOOOOOkxdlldc;;;:;;,;;;;;;;;;;:xO0W   WO::kW               WWWWWWWWWWOXNNNNNNNKkxddkkdoddxxl:kNWW     WWX0kxxX     WWW0ook0KNWW           
+       Nkok0OOOxd0c,O     WWNK0Okdc'..cOOOOOOOkkkkkOk;;;;::;,,;;;;;:ldOOOW     Wx:cX                      WWOdNNNNNNNNNNKxxkddddxxxxdcldOXW W       W      WWOdldNWW              
+       WWN0xxO00OXKlxWXKXOoo:........,:kOOOOOOOOOOOOx;;;;;;;;,:cldxOOkk0WW     WXc:O                   WWW0dooXNNNNNNNNNNNNNkodxxxxxxxoododxO0XWWWWW  WWWWX0xdcoXWWW              
+        WWWNOxOOxdxxxx:.......',:::;;;;ckOOOOOOOOOOOd;;;;;;;;;cOOOkxxONW        Nl:O                   WXdooold0XNNNNNNNNNNXxxxxxxxxxxxddkxxxdodxkkOO0OOOxddlcxNW                 
+             WXNNNXK0kxddxkO0KXNO::;;;;;:xOOOOOOOOOOd;;;;;;;;;'oddkKW           WlcN                  WXllllllclxO000KKKK00ddxxddxxxxxxxoxxxkkkkxxxxxxxxdolldKW                   
+                              W0:;;;;,,'.':oxxxxxxxxd;;;;::;,'.'cKW            WWKXW                  W0cllllllllodkOOOOOOOxdxxxxxxxkxxdcdddxxxxddddooooldkKWW                    
+                              W0;''''.......okdooodddo;''''......:N                                    WOdccllllllccdOkkkOOOddxxxxxddoll:ldddddddoodxO00XWW                       
+                              WWOl;''......kWWWWNXXXXXXKxlc;......lWW                                  WWWXl;::::;c0WWWNXKKKK0xllllllcllcclOKKKKXXNWWW                            
+                                WWWXl.....cW               0,.....'0W                                WN0xolccc:;;;:xWW        WXkdlccllll:cK                                      
+                               WNkdoo;:;..OW               0;,,,,,,dW                              WX0kkdoocloddxk0NW           WWXllclllcckW                                     
+                               WWKK0O0KKOXWW             WKk0okOlkcxW                             WWWNXK0KXNWWWWW               WKxclolcod:oN                                     
+                                                        WN0N0ON00NXWW                                                         WW0KKOkOkO0NoOW                                     
+                                                                                                                              WWWWW NWWWNXXW                                      
+                                                                                                                                       W                                          
+                                                                                                                                                                                  
+
+EOF
