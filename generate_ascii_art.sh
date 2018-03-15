94c66a9a8c57acf5e6d7391f3a0feffae202f328
Keon Ye <jnuyeyizhou@gmail.com>
2018-03-15 23:47:25 +0800

    add some comment for usage

--- generate_ascii_art.sh
+++ generate_ascii_art.sh
@@ -8,4 +8,9 @@ else
   echo "The ascii_pictures directory has been existing in your system."
 fi
 
+# execute this command to generate the ascii art
+# Usage:
+#    jp2a picture_file_name
+#
+# Note: picture_file_name should be existed in the picture directory
 jp2a "pictures"/$1
