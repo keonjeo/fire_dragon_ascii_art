93bc024f69d436d06ae635a6152b2a3d5eb6ca02
Keon Ye <jnuyeyizhou@gmail.com>
2018-03-16 01:21:02 +0800

    update the generate_ascii_art.sh script

--- generate_ascii_art.sh
+++ generate_ascii_art.sh
@@ -15,12 +15,28 @@ fi
 # Note: picture_file_name should be existed in the picture directory
 jp2a "pictures"/$1 > ascii_pictures/temp.txt
 
+# get its file_name_without_suffix_name
+file_name=$1
+new_file_name=${file_name%.*}
 
+echo "#!/bin/bash --login" > ascii_pictures/$new_file_name.sh
+echo "clear" >> ascii_pictures/$new_file_name.sh
+echo "cat << \"EOF\"" >> ascii_pictures/$new_file_name.sh
 
 # use the tr bin to filter 'M' character
-cat ascii_pictures/temp.txt | tr "M" " " > ascii_pictures/hello.txt
+cat ascii_pictures/temp.txt | tr "M" " " >> ascii_pictures/$new_file_name.sh
+
+echo "EOF" >> ascii_pictures/$new_file_name.sh
 
 
 # display the ascii art on the screen
-cat ascii_pictures/hello.txt
+cat ascii_pictures/$new_file_name.sh
+
+# change its permission, make it can be executed
+chmod a+x ascii_pictures/$new_file_name.sh
+
+# git commands
+git add ascii_pictures/$new_file_name.sh
+git commit -m "add $new_file_name.sh script into the project"
+
 
